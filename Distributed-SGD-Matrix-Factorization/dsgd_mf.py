48774b54c33500a5326e01a1df070db4e886bbd6
niloygupta <niloygupta@gmail.com>
2015-04-09 15:09:22 -0400

    Adding config parameters

--- Distributed-SGD-Matrix-Factorization/dsgd_mf.py
+++ Distributed-SGD-Matrix-Factorization/dsgd_mf.py
@@ -9,7 +9,7 @@ from pyspark import SparkContext
 def main():
     sc = SparkContext("local", "SGD-Matrix")
     words = sc.textFile("/Users/niloygupta/spark-1.3.0/README.md")
-    a = words.filter(lambda w: w.startswith("spar")).take(5)
+    a = words.filter(lambda w: w.startswith("Spar")).take(5)
     print a
 
 if __name__ == '__main__':
