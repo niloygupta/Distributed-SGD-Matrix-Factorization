0c57e53c349f84704f43057ebd109db92ebd0d6f
niloygupta <niloygupta@gmail.com>
2015-04-16 23:12:26 -0400

    THE END

--- Distributed-SGD-Matrix-Factorization/testing.py
+++ Distributed-SGD-Matrix-Factorization/testing.py
@@ -4,52 +4,59 @@ from pyspark import SparkContext, SparkConf
 import math 
 import numpy as np
 import random
+from scipy.sparse import csr_matrix
 
 def dotProduct(M):
-	return np.sum(M * M)
+	return np.sum(np.multiply(M,M))
 
 def computeGradient(block):
 	#block = block.collect()
-	Vmini, Wmini, Hmini, globalIter, index = block[0], block[1], block[2], block[3], block[4]
+	blockInfo, Wmini, Hmini, globalIter, index = block[0], block[1], block[2], block[3], block[4]
 	blockEntryRow = block[5]
 	blockEntryCol = block[6]
-	#Vmini = V.collect()
-	total = len(Vmini) 
+	
 
+	V = getSparseMatrix(blockInfo,Wmini.shape[0],Hmini.shape[1],blockEntryRow,blockEntryCol)
+	#Vmini = V.collect()
+
+	rows,cols = V.nonzero()
+	total = len(rows) 
 	iteration = 0
 	prevLoss = -1.0
-	nonzeroCount = len(Vmini)
+	nonzeroCount = len(rows)
 	while True and nonzeroCount > 0:
 		randomIndex = random.randint(0, total-1)
-		
-		r = Vmini[randomIndex][1][0] - blockEntryRow
-		c = Vmini[randomIndex][1][1] - blockEntryCol
+		r = rows[randomIndex]
+		c = cols[randomIndex]
 
 		W = Wmini[r, :]
 		H = Hmini[:, c]
 		learningRate = math.pow((100 + globalIter +  iteration), -betaValue)
 		
 
-		temp = 2.0 * (Vmini[randomIndex][1][2] - W.dot(H))
+		temp = 2.0 * (V[r,c] - W.dot(H))
 
 		#vrnonzeros = 1.0 * V.filter(lambda x: x[0] == r).count()
 		#vcnonzeros = 1.0 * V.filter(lambda x: x[1] == c).count()
 		
-		vrnonzeros = 1.0 * len(map(lambda x: x[1][0] == r, Vmini))
-		vcnonzeros = 1.0 * len(map(lambda x: x[1][1] == c, Vmini))
+		#vrnonzeros = 1.0 * len(map(lambda x: x[1][0] == r, Vmini))
+		#vcnonzeros = 1.0 * len(map(lambda x: x[1][1] == c, Vmini))
+		
+		vrnonzeros = V[r,:].nonzero()[0].size
+		vcnonzeros = V[:,c].nonzero()[0].size
 		
 		W += (learningRate * temp * H.T) - (2.0 * learningRate * (lambdaValue/vrnonzeros) * W)
 		H += (learningRate * temp * Wmini[r, :].T) - (2.0 * learningRate * (lambdaValue/vcnonzeros) * H)
 		Wmini[r, :] = W
 		Hmini[:, c] = H
-		#loss = Vnonzero - Wmini.dot(Hmini)[rowZ, colZ]
-		#loss = dotProduct(loss) + lambdaValue * (dotProduct(Wmini) + dotProduct(Hmini))
-		#if np.fabs(prevLoss - loss) < 0.000001:
-		#	break
-		#else:
-		#	prevLoss = loss
+		loss = V - Wmini.dot(Hmini) #[rows, cols]
+		loss = dotProduct(loss) + lambdaValue * (dotProduct(Wmini) + dotProduct(Hmini))
+		if np.fabs(prevLoss - loss) < 0.000001:
+			break
+		else:
+			prevLoss = loss
 		iteration += 1
-		if iteration == 1000:
+		if iteration == 3000:
 			break
 	
 	return (index, globalIter + iteration, Wmini, Hmini)
@@ -120,7 +127,7 @@ def main():
 	for iteration in xrange(noOfIterations):
 		
 		globalIter = 0
-		for stratum in range(2,noOfWorkers):#partitions.keys().collect():
+		for stratum in range(0,noOfWorkers):#partitions.keys().collect():
 			blocks = partitions.filter(lambda x: x[0] == stratum)
 
 			temp = stratumIndices[stratum]
@@ -129,20 +136,11 @@ def main():
 				blockEntryRow, blockExitRow, blockEntryCol, blockExitCol = block[0], block[1], block[2], block[3]
 				Wmini = W[blockEntryRow:blockExitRow, :]
 				Hmini = H[:, blockEntryCol:blockExitCol]
-				blockInfo = blocks.filter(lambda x: x[1][0]/blockSize[1] == index)			
+				blockInfo = blocks.filter(lambda x: x[1][0]/blockSize[0] == index)
 
-				
-				#exit(0)
-				#blockInfo = blockInfo.map(lambda x: (x, Wmini, Hmini, globalIter, index, blockEntryRow, blockEntryCol))
-				#print blockInfo.collect()
-				#exit(0)
+				#V = getSparseMatrix(blockInfo,blockEntryRow,blockEntryCol)
 				blockInfoList.append((blockInfo.collect(), Wmini, Hmini, globalIter, index, blockEntryRow, blockEntryCol))
-				#blockInfoList.append(blockInfo)
-			
 			result = sc.parallelize(blockInfoList, noOfWorkers).map(lambda x: computeGradient(x)).collect()
-			#result = blockInfoList.par.foreach(computeGradient()).collect()
-			#result = blockInfoList.map(computeGradient()).collect()
-			#result = result.map(lambda x: computeGradient(x)).collect()
 			for r in result:            # Updating W, H from the partial results of each worker
 				blockNum = r[0]
 				block = temp[blockNum]
@@ -155,6 +153,21 @@ def main():
 	writeOutput(H, outputHPath)
 	#writeOutput(W.dot(H), "predicted_ratings.csv")
 	sc.stop()
+
+def getSparseMatrix(blockInfo,rowSize,colSize,blockEntryRow,blockEntryCol):
+	'''
+	rows =  map(lambda x:x[1][0]-blockEntryRow,blockInfo).collect()
+	cols = map(lambda x:x[1][1]-blockEntryCol-blockInfo).collect()
+	ratings = blockInfo.map(lambda x:-1 if x[1][2]==0 else x[1][2]).collect()'''
+	rows = []
+	cols = []
+	ratings = []
+	for triple in blockInfo:
+		rows.append(triple[1][0]-blockEntryRow)
+		cols.append(triple[1][1]-blockEntryCol)
+		ratings.append(-1 if triple[1][2]==0 else triple[1][2])
+	
+	return csr_matrix((ratings, (rows,cols)), shape=(rowSize, colSize))
 	
 if __name__ == "__main__":
 	main()
